0ce3754d83f41c40130d352c6a9d095b2ea1e578
ManuelBasso <146716888+ManuelBasso@users.noreply.github.com>
2023-10-10 14:34:20 +0200

    Add files via upload

---
+++ 6_Methods.java
@@ -0,0 +1,18 @@
+//Definire una funzione che accetti in ingresso una stringa e ne restituisca la lunghezza
+
+public class Main {
+    public static void main(String args[]) {
+    String name = "Manuel";
+    
+    System.out.println("Lunghezza stringa = " + stringLength (name) );
+    
+    }
+    
+    
+    public static int stringLength (String a) {
+    return  a.length();
+ 
+ }
+    
+}
+
